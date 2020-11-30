bd2608f2e0b069c12d6c529a52406e9c76ef3ab0
GuilhermeDallari <guilhermdallarii@gmail.com>
2020-11-30 08:36:05 -0300

    Update ex006.py

--- ex006.py
+++ ex006.py
@@ -2,4 +2,4 @@ n1 = int(input('digite um numero: '))
 m = n1 * 2
 r = n1 * 3
 r1 = n1 ** (1/2)
-print('o dobro de {} vale {}, o triplo vale {} e a raiz vale {:.2f}'.format(n1, m, r, r1))
+print('o dobro de {} vale {} \n o triplo vale {}\n e a raiz vale {:.2f}'.format(n1, m, r, r1))
