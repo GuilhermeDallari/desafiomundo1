08ad3391c71a9491e38966585521c5372618b048
GuilhermeDallari <guilhermdallarii@gmail.com>
2020-12-01 04:55:32 -0300

    Create ex015.py

---
+++ ex015.py
@@ -0,0 +1,7 @@
+n1 = float(input('quantos dias você alugou o veiculo? '))
+n2 = float(input('quantos km você rodou? '))
+d1 = n1*50
+d2 = n2*0.25
+total = d1+d2
+print('o valor total é de R$ {}'.format(total))
+
