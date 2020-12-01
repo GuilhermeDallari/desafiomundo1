fec4e0161c6933da1136cee79c4f123d347b8c04
GuilhermeDallari <guilhermdallarii@gmail.com>
2020-12-01 04:45:35 -0300

    python

---
+++ ex013.py
@@ -0,0 +1,3 @@
+n1 = float(input('descubra agora quanto vai receber apos o reajuste, digite seu salario em R$: '))
+n2 = n1 + (n1*10/100)
+print('o seu salario com o novo reajuste de 10% sera R${:.2f}'.format(n2))
---
+++ ex014.py
@@ -0,0 +1,3 @@
+n1 = float(input('Temperatura em °C: '))
+n2 = n1*9/5 + 32
+print('{:.1f}°C corresponde a {:.1f} °F!'.format(n1, n2))
